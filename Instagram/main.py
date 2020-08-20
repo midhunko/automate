9a1640efa1c50794b4df77e541b6fdfc9c2b859d
Midhun KO <midhunko1996@outlook.com>
2020-08-20 12:43:24 +0530

    ig automate finished

--- Instagram/main.py
+++ Instagram/main.py
@@ -11,5 +11,13 @@ time.sleep(1)
 
 for user in users:
     browser.get(f"https://www.instagram.com/{user}")
-    post, followers, following = browser.find_element_by_css_selector('.g47SY')
+    time.sleep(1)
+    post, followers, following = browser.find_elements_by_css_selector('.g47SY')
+    bio = browser.find_element_by_css_selector('.-vDIg')
+    with open(f"{user}.txt", "w", encoding="utf-8") as f:
+        f.write(f"No of posts: {post.text}\nNo of followers: {followers.text}\nNo of following: {following.text}\n\nBio:\n{bio.text}")
+    time.sleep(2)  
+
+browser.close()
+
 
